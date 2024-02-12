6fb706528b8192ffc4781fdf4644ca27238a2789
jyotiops <65543404+jyotiops@users.noreply.github.com>
2024-02-12 17:35:01 +0530

    new

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Runtime.InteropServices;
-using Microsoft.ApplicationInsights;
+
 
 namespace dotnetcoresample.Pages;
 
